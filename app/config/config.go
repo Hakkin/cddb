dfce26a2970a93badb346aaf233de14760ec56c9
Hakkin Lain <hakkin@github>
2019-09-05 07:30:25 -0700

    Update config to use env variables or command line switches.

--- app/config/config.go
+++ app/config/config.go
@@ -1,6 +1,24 @@
 package config
 
-const (
-	Client = "XXXXXXX-XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
-	User   = "XXXXXXXXXXXXXXXXXX-XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+import (
+	"flag"
+	"os"
 )
+
+var (
+	Client string
+	User   string
+)
+
+func init() {
+	if clientEnv := os.Getenv("CLIENT_KEY"); clientEnv != "" {
+		Client = clientEnv
+	}
+
+	if userEnv := os.Getenv("USER_KEY"); userEnv != "" {
+		User = userEnv
+	}
+
+	flag.StringVar(&Client, "client", Client, "Gracenote Client ID")
+	flag.StringVar(&User, "user", User, "Gracenote User ID")
+}
