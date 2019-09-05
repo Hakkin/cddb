1c871899963ca4eb8bc284e49d032ee675206985
Hakkin Lain <hakkin@github>
2019-09-05 06:48:32 -0700

    Remove build files.

--- app/client/build-windows.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-SET GOOS=windows
-SET GOARCH=amd64
-SET ldflags=-s -w
-SET output=./build/windows
-SET binary="cddb.exe"
-
-go build -v -o "%output%/%binary%" -ldflags "%ldflags%" standalone.go
